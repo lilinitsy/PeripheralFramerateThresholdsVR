5a15aefc54f680eeb05b143440cec4fd31838ee3
ville <lilinitsy@gmail.com>
2025-01-09 16:12:42 -0600

    Initial commit

---
+++ Assets/src/ExperimentParams.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
+public class NewScriptableObjectScript : ScriptableObject
+{
+    public int target_framerate = 90; // 120 for Quest 3? 
+}
